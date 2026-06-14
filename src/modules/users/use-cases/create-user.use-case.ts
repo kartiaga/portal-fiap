f65bd966ed416ba0f30b5cf3cd7753cfeab1950f
Kaique Luz <kaiqueartiagaluz@gmail.com>
2026-06-14 14:02:57 -0300

    feat(password): saving the password hashed on database

--- src/modules/users/use-cases/create-user.use-case.ts
+++ src/modules/users/use-cases/create-user.use-case.ts
@@ -1,10 +1,16 @@
+import { hashPassword } from "@/lib/password";
 import type { User } from "../entities/user";
 import type { UserRepository } from "../repositories/user.repository";
 
 export class CreateUserUseCase {
     constructor(private readonly userRepository: UserRepository) {}
 
-    handler(user: User): Promise<User | undefined> {
-        return this.userRepository.create(user)
+    async handler(user: User): Promise<User | undefined> {
+        const hashedPassword = await hashPassword(user.password)
+        
+        return this.userRepository.create({
+            ...user,
+            password: hashedPassword
+        })
     }
 }
