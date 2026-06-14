7bb51b2b576ab62576eead9fd46b3ad4f046d297
Kaique Luz <kaiqueartiagaluz@gmail.com>
2026-06-14 07:58:24 -0300

    feat(migrations): node-pg-migration para install, scripts to up, down and create migrations. Creation of table users

---
+++ migrations/1781399377280_create-users-table.js
@@ -0,0 +1,47 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable("users", {
+      id: {
+        type: "uuid",
+        primaryKey: true,
+        default: pgm.func("gen_random_uuid()"),
+      },
+      email: {
+        type: "varchar(255)",
+        notNull: true,
+        unique: true,
+      },
+      password: {
+        type: "varchar(255)",
+        notNull: true,
+      },
+      created_at: {
+        type: "timestamptz",
+        notNull: true,
+        default: pgm.func("now()"),
+      },
+      updated_at: {
+        type: "timestamptz",
+        notNull: true,
+        default: pgm.func("now()"),
+      },
+    });
+  };
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable("users")
+};
