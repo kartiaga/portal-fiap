42528f72807af029bbb4f1a89ed9c58a63cee4b8
teste <teste>
2026-06-25 19:18:12 -0300

    fix: Fix workflow error

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]sx?$': ['ts-jest', { useESM: true, tsconfig: '<rootDir>/tsconfig.json' }],
+    },
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+}
